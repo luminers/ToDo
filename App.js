f4920cea336bb433c99207c9f2c898141d3e9d8d
Joaquin nunez <magnanimoysimple@gmail.com>
2022-04-11 20:22:30 -0300

    Funcion de añadir tarea

--- App.js
+++ App.js
@@ -8,22 +8,46 @@ $btnAgregar=document.getElementById("agregar"),
 $radioButtons=document.querySelectorAll(`input[type="radio"]`);
 
 
+
 $btnAgregar.addEventListener("click",  () =>{
     let estado;
     for (const radioButton of $radioButtons) {
      if (radioButton.checked) {
          estado = radioButton.id;
           break;
-      }else{
-          console.log("No hay")
       }
      }
+
+    let tarea= crearTarea($nombre.value,$descripcion.value,estado);
+     //$fragmento.appendChild(tarea);
+
+     switch (estado) {
+        case "pendiente":
+            $listaPendiente.innerHTML+=tarea;
+            break;
+        case "enProgreso":
+             $listaPendiente.innerHTML+=tarea;
+             break;
+        case "finalizada":
+             $listaPendiente.innerHTML+=tarea;
+             break;
      
- 
-
-
+         default:
+             break;
+     }
 
 
 });
+
+    
+
+
+
+function crearTarea(nombre, descripcion, estado) {
+    return `<div  id="tarea" class="tarea${estado}">
+        <h3 class="nombre-tarea">${nombre}</h3>
+        <p class="descripcion-tarea">${descripcion}</p>
+    </div>`;
+}
 
 
